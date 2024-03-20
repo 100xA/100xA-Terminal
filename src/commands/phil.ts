8b9fbf618b2a9199da3cf181df47987b2bcf4589
JanAppAkademie <jan.biernacki@app-akademie.com>
2024-03-20 08:54:48 +0100

    Add Button and Change Inputs

--- src/commands/phil.ts
+++ src/commands/phil.ts
@@ -16,6 +16,24 @@ const philObj = {
       "As contraries are known by contraries, so is the delight of presence best known by the torments of absence.",
       "- Alcibiades",
     ],
+    [
+      "Neither pleasure nor pain should enter as motives when one must do what must be done.",
+      "- J. Evola",
+    ],
+    ["He who thinks great thoughts often makes great errors.", "- M.Heidegger"],
+    ["Man dies constantly until the moment of his demise.", "- M.Heidegger"],
+    [
+      "I am the master! I stretch forth my hands, even to the skies! I lay my hands upon the stars, as on the crystal wheels of the harmonica. Now fast, now slow, as my soul wills, I turn the stars. I weace them into rainbows, harmonies. I feel immortality! I create immortality!",
+      "- A.Mickiewicz",
+    ],
+    [
+      "Pain and suffering are always inevitable for a large intelligence and a deep heart. The really great men must, I think, have great sadness on earth.",
+      "- F.Dostoyevsky",
+    ],
+    [
+      "The peoples of the world do not invent their gods. They deify their victims.",
+      "- R.Girard",
+    ],
   ],
 };
 
--- src/commands/projects.ts
+++ src/commands/projects.ts
@@ -8,6 +8,7 @@ projectMap.set("Pomodoro Tracker", [
   "https://100xa.github.io/2023/03/22/project-pomodoro-tracker/",
   "CLI AppleScript Pomodoro Tracker",
 ]);
+
 const createProject = (): string[] => {
   const projects: string[] = [];
   const SPACE = " ";
@@ -17,7 +18,7 @@ const createProject = (): string[] => {
   projectMap.forEach((value, projectName) => {
     const [link, description] = value;
     const linkElement = `<a href="${link}" target="_blank">${projectName}</a>`;
-    const padding = SPACE.repeat(30 - projectName.length); // Increased spacing here
+    const padding = SPACE.repeat(30 - projectName.length);
     const projectString = `${SPACE.repeat(
       2
     )}${linkElement} -${padding}${description}`;
